0e2a6a4fdacd334fea8c7f292a86c3e9a033fc97
Nguyen Hoang Khoi <183231218+nhapHoangKhoi@users.noreply.github.com>
2026-08-12 17:03:00 +0700

    Initialize project

---
+++ src/main/java/com/hoangkhoi/springboot_book_generate_images/SpringbootBookGenerateImagesApplication.java
@@ -0,0 +1,13 @@
+package com.hoangkhoi.springboot_book_generate_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBookGenerateImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBookGenerateImagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoangkhoi/springboot_book_generate_images/SpringbootBookGenerateImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangkhoi.springboot_book_generate_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBookGenerateImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
