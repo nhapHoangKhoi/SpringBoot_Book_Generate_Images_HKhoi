a2a3ffca59be6142d8bd88df7cf01aa0c869b5cb
Nguyen Hoang Khoi <183231218+nhapHoangKhoi@users.noreply.github.com>
2026-08-12 17:05:52 +0700

    Set up SwaggerUI for API documentation

--- src/main/java/com/hoangkhoi/springboot_book_generate_images/SpringbootBookGenerateImagesApplication.java
+++ src/main/java/com/hoangkhoi/springboot_book_generate_images/SpringbootBookGenerateImagesApplication.java
@@ -2,9 +2,17 @@ package com.hoangkhoi.springboot_book_generate_images;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootBookGenerateImagesApplication {
+
+    @GetMapping("/hello")
+    String sayHello() {
+        return "Hello Spring Boot, Khoi!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBookGenerateImagesApplication.class, args);
