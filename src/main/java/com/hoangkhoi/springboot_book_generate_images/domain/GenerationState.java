c27350b5efb99a5b06d153627a4122fec7641726
Nguyen Hoang Khoi <183231218+nhapHoangKhoi@users.noreply.github.com>
2026-08-12 19:52:22 +0700

    Define enum states

---
+++ src/main/java/com/hoangkhoi/springboot_book_generate_images/domain/GenerationState.java
@@ -0,0 +1,8 @@
+package com.hoangkhoi.springboot_book_generate_images.domain;
+
+public enum GenerationState {
+    PENDING,
+    RUNNING,
+    COMPLETED,
+    FAILED
+}
---
+++ src/main/java/com/hoangkhoi/springboot_book_generate_images/domain/Step.java
@@ -0,0 +1,9 @@
+package com.hoangkhoi.springboot_book_generate_images.domain;
+
+public enum Step {
+    STYLE,
+    CHARACTERS,
+    PORTRAITS,
+    CHAPTERS,
+    ILLUSTRATIONS
+}
