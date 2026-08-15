866a14c0ca4b45673fd454df67e159ea460bd02c
Nguyen Hoang Khoi <183231218+nhapHoangKhoi@users.noreply.github.com>
2026-08-15 22:20:10 +0700

    Hotfix project controller

--- src/main/java/com/hoangkhoi/springboot_book_generate_images/controller/ProjectController.java
+++ src/main/java/com/hoangkhoi/springboot_book_generate_images/controller/ProjectController.java
@@ -11,15 +11,21 @@ import com.hoangkhoi.springboot_book_generate_images.response.ApiResponse;
 import com.hoangkhoi.springboot_book_generate_images.response.SuccessMessages;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import java.time.Instant;
+import java.util.List;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.Instant;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
